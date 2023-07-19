725c388b44eda07db9d68436534644753257c62a
tlsrhksgh <sin99145@naver.com>
2023-07-19 22:18:59 +0900

    refactor: httpstatus code 수정 400 -> 404

--- src/main/java/com/single/springboard/exception/ErrorCode.java
+++ src/main/java/com/single/springboard/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    NOT_EXIST_POST(HttpStatus.BAD_REQUEST, "해당 게시물이 존재하지 않습니다.");
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String content;
