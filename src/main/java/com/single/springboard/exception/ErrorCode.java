ce2a4c0ccdf82b4478cb227b08ec322b60a64efc
tlsrhksgh <sin99145@naver.com>
2023-07-24 01:47:31 +0900

    feature: User exception 추가

--- src/main/java/com/single/springboard/exception/ErrorCode.java
+++ src/main/java/com/single/springboard/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다.");
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다."),
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 회원이므로 회원 가입이 필요합니다.");
 
     private final HttpStatus httpStatus;
     private final String content;
