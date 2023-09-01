c178ac5d6fc92369e5189acc68ed283c3ec07131
tlsrhksgh <sin99145@naver.com>
2023-09-01 16:55:56 +0900

    modified:메인 페이지와 게시글 read 페이지에 fetch join을 이용한 성능 최적화

--- src/main/java/com/single/springboard/domain/post/PostRepository.java
+++ src/main/java/com/single/springboard/domain/post/PostRepository.java
@@ -4,9 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
-    @Query("SELECT p, COUNT(c) FROM Post p LEFT JOIN Comment c ON p.id = c.post.id GROUP BY p ORDER BY p.id DESC ")
-    Page<Object[]> findAllPostsWithCommentsCount(Pageable pageable);
+    @Query("SELECT p, COUNT(pc) FROM Post p " +
+            "LEFT JOIN FETCH p.comments pc " +
+            "LEFT JOIN FETCH p.user " +
+            "GROUP BY p " +
+            "ORDER BY p.id DESC")
+    Page<Object[]> findAllPostsWithCommentsCountAndUser(Pageable pageable);
 
+    @Query("SELECT p from Post p LEFT JOIN FETCH p.comments c LEFT JOIN FETCH c.user where p.id = :postId")
+    Post findPostWithCommentsAndUser(@Param("postId") Long postId);
 }
